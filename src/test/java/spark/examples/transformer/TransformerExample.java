512fc19c0fa77b3f35ffde12099ca3caedcc7532
MatsuyamaHiroshi <matsuyama@orb-japan.co.jp>
2016-07-06 12:07:22 +0900

    Transformerクラスを使わず、直接Gsonから出力するようにしてみた。

--- src/test/java/spark/examples/transformer/TransformerExample.java
+++ src/test/java/spark/examples/transformer/TransformerExample.java
@@ -4,16 +4,21 @@ import static spark.Spark.get;
 
 import java.util.HashMap;
 
+import com.google.gson.Gson;
+
 public class TransformerExample {
 
     public static void main(String args[]) {
+    	
+    	Gson gson = new Gson();
+    	
         get("/hello", "application/json", (request, response) -> {
             //return new MyMessage("Hello World");
         	HashMap<String, String> map = new HashMap<String, String>();
         	map.put("greet","こんにちは");
-        	map.put("to","世界の皆さん");
+        	map.put("to","日本の皆さん");
         	return map;
-        }, new JsonTransformer());
+        }, gson::toJson);
     }
 
 }
