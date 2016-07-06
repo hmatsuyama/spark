63f7c2ea48976e3b7f8fbf295cccd794f5d625fa
MatsuyamaHiroshi <matsuyama@orb-japan.co.jp>
2016-07-06 12:02:04 +0900

    MyMessageオブジェクトをHashMapに変えてみた。

--- src/test/java/spark/examples/transformer/TransformerExample.java
+++ src/test/java/spark/examples/transformer/TransformerExample.java
@@ -2,11 +2,17 @@ package spark.examples.transformer;
 
 import static spark.Spark.get;
 
+import java.util.HashMap;
+
 public class TransformerExample {
 
     public static void main(String args[]) {
         get("/hello", "application/json", (request, response) -> {
-            return new MyMessage("Hello World");
+            //return new MyMessage("Hello World");
+        	HashMap<String, String> map = new HashMap<String, String>();
+        	map.put("greet","こんにちは");
+        	map.put("to","世界の皆さん");
+        	return map;
         }, new JsonTransformer());
     }
 
